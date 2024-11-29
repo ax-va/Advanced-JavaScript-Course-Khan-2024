9fd40d5babf9559cd8cf44519c8bd7275089881a
ax-va <vasiliev.informatik@t-online.de>
2024-11-29 23:45:59 +0100

    update

--- 01--hoisting/01-1--var-declaration.js
+++ 01--hoisting/01-1--var-declaration.js
@@ -1,10 +1,14 @@
+/*
+Hoisting = variables and functions can be accessed *before* their actual declaration
+ */
+
 // The access to undeclared variables is possible
 // because of the parsing step before the code is executed.
 console.log(result);
 // undefined
 var result = 5 + 10;
 
-// Some errors are detected in the preprocessing step before the code is executed
+// In general, some errors are detected in the preprocessing step before the code is executed
 function print(obj) {
     // Make error
     console.log(obj;
